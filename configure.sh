ade160102d7b4474a1b49f8b331c4dedf058392f
Merlin Danner <merlin.danner@fau.de>
2020-06-05 08:30:40 +0200

    Building on windows works.

--- configure.sh
+++ configure.sh
@@ -3,4 +3,4 @@ dir=$(dirname "$(readlink -f $0)" )
 arg=""
 echo $EDITOR | grep vim -q && arg="-DCMAKE_EXPORT_COMPILE_COMMANDS=1"
 mkdir -p "$dir/build"
-cmake -S "$dir" -B "$dir/build" $arg $@
+cmake -S "$dir" -B "$dir/build" $arg "$@"
--- include/b64_encode.h
+++ include/b64_encode.h
@@ -1,4 +1,4 @@
-
+#pragma once
 #include <string>
 
 std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int length);
