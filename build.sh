97fbe50d40c72613c54897d02e0abca2840eebe0
Merlin Danner <merlin.danner@fau.de>
2020-06-05 10:21:35 +0200

    move dependency library to binary

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ die() {
 
 [ "$OS" == "Windows_NT" ] || [ -f "$dir/build/Makefile" ] || "$dir/configure.sh" $@
 
-cmake --build "$dir/build" -j4
+cmake --build "$dir/build" -j4 && \
+mv "$dir/build/deps/libbeatcppver.dll" "$dir/build"
